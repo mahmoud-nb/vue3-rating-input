eb4d92f56afd7c4cebe979e20fc94b13a8b38d04
Mahmoud <mahmoud.nb@gmail.com>
2024-03-10 14:01:28 +0100

    feat: Improve unit testing

--- src/spec/components/rating-input.spec.ts
+++ src/spec/components/rating-input.spec.ts
@@ -1,50 +1,75 @@
-import { mount } from '@vue/test-utils'
+import { ref, computed } from 'vue'
+import { mount, shallowMount } from '@vue/test-utils'
 import { describe, it, expect } from 'vitest'
 import RatingInputComponent from '../../components/rating-input.vue'
+import StarSvgIcon from '../../components/star-svg-icon.vue'
 
 const getRandomIndex = (max:number): number => Math.floor(Math.random() * max);
 
+describe('Vue Rating Input', () => {
 
-describe('Vue Rating Input: default props', () => {
+  const RatingInputWrapper = mount(RatingInputComponent)
+  const stars = RatingInputWrapper.findAll('.rating-input__item')
 
-    const wrapper = mount(RatingInputComponent)
-
-    it('shoud render Rating Input component', () => {
-        expect(wrapper.find('.rating-input').exists()).toBeTruthy()
-    })
-    
-    it('shoud load Rating Input component template', () => {
-        expect(wrapper.html()).toMatchSnapshot()
+  describe('Star Svg Icon Component', () => {
+    const StarSvgIconWrapper = shallowMount(StarSvgIcon);
+    it('renders correctly', () => {
+        expect(StarSvgIconWrapper.exists()).toBe(true);
     })
 
-    it('shoud load Rating Input component with 5 stars', () => {
-        const stars = wrapper.findAll('.rating-input__item').length
+    it('changes color when hovered', async () => {
+        await stars[0].trigger('mouseenter')
+        const svg = StarSvgIconWrapper.find('svg')
+        expect(svg.find('use').attributes().href).toEqual('#solid')
+    })
+
+    it('emits update:modelValue when clicked', async () => {
+        await stars[0].trigger('click')
+        expect(RatingInputWrapper.emitted('update:modelValue')).toBeTruthy();
+    })
+  })
+
+  describe('Vue Rating Input Component', () => {
+      
+    describe('With default props', () => {
     
+      it('shoud render Rating Input component', () => {
+        expect(RatingInputWrapper.find('.rating-input').exists()).toBeTruthy()
+      })
+      
+      it.skip('shoud load Rating Input component template', () => {
+        expect(RatingInputWrapper.html()).toMatchSnapshot()
+      })
+  
+      it('shoud load Rating Input component with 5 stars', () => {
+        const stars = RatingInputWrapper.findAll('.rating-input__item').length
         expect(stars).toEqual(5)
-    })
+      })
+    
+      it('shoud load Rating Input component with 5 stars', async () => {
+  
+        const randomSelectStar = ref(getRandomIndex(6))
+        const randomSelectStarValue = computed(() => randomSelectStar.value + 1) 
 
-    it('shoud load Rating Input component with 5 stars', async () => {
-        const stars = wrapper.findAll('.rating-input__item')
+        const stars = RatingInputWrapper.findAll('.rating-input__item')
 
-        const randomSelectStar = getRandomIndex(6)
-        const randomSelectStarValue = randomSelectStar + 1
+        await stars[randomSelectStar.value].trigger('click')
 
-        await stars[randomSelectStar].trigger('click')
+        // @ts-ignore
+        expect(RatingInputWrapper.vm.selectedStar).toEqual(randomSelectStarValue.value)
 
-        expect(wrapper.vm.selectedStar).toEqual(randomSelectStarValue)
-
-        const emitedValue = wrapper.emitted()['update:modelValue'] // wrapper.emitted() : { "update:modelValue": [ [randomSelectStarValue] ] }
+        const emitedValue = RatingInputWrapper.emitted()['update:modelValue']
 
         expect(emitedValue).toBeTruthy()
 
-        expect(emitedValue[0]).toEqual([randomSelectStarValue])
+        expect(emitedValue[randomSelectStar.value]).toEqual([randomSelectStarValue.value])
+      })
     })
-})
 
-describe('Vue Rating Input: custom props', () => {
+    describe('With custom props', () => {
 
-    it('shoud load Rating Input component with 3 stars', () => {
-
+      it('shoud load Rating Input component with 3 stars', () => {
+  
         const numberOfStars = 3
 
         const wrapper = mount(RatingInputComponent, {
@@ -56,18 +81,28 @@ describe('Vue Rating Input: custom props', () => {
         const stars = wrapper.findAll('.rating-input__item').length
     
         expect(stars).toEqual(numberOfStars)
-    })
-
-    it('shoud init selectedStar with props modelValue', () => {
-
-        const modelValue = 2
+      })
+  
+      it('shoud init selectedStar with props modelValue', () => {
+  
+        const modelValue = ref(2)
 
         const wrapper = mount(RatingInputComponent, {
             propsData: {
-                modelValue
+                modelValue: modelValue.value
             }
         })
-    
-        expect(wrapper.vm.selectedStar).toEqual(modelValue)
+      
+        // @ts-ignore
+        expect(wrapper.vm.selectedStar).toEqual(modelValue.value)
+      })
     })
+  })
 })
+
+
+
+
+
+
+
